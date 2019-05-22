24bdece0baf6ee49dd6114ef718deb0502c42ffa
Laoshuatou-AlaSKa <752779922@qq.com>
2019-05-22 18:11:28 +0800

    2019-5-22 18:10 提交者：IDEA程序员 提交内容：Jiaowu.java、Student.java 目标分支；feature1

---
+++ src/com/alaska/Main.java
@@ -0,0 +1,16 @@
+package com.alaska;
+
+/**
+ * 功能描述:
+ *
+ * @ClassName: Main
+ * Workstation:AlaSKa_
+ * @Author: Laoshuaitou-AlaSKa
+ * @Date: 2019/5/22 18:08
+ * @Version:
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("good branch!");
+	}
+}
