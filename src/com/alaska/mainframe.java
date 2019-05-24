6d61f9192170b756c768597af6c9228a5f524639
Laoshuatou-AlaSKa <752779922@qq.com>
2019-05-24 10:35:28 +0800

    2019-5-24 10:34 提交者：IDEA程序员 提交内容：V2.0（tag） 目标分支；develop

---
+++ src/com/alaska/mainframe.java
@@ -0,0 +1,13 @@
+package com.alaska;
+
+/**
+ * 功能描述:
+ *
+ * @ClassName: mainframe
+ * Workstation:AlaSKa_
+ * @Author: Laoshuaitou-AlaSKa
+ * @Date: 2019/5/24 10:32
+ * @Version:V2.0
+ */
+public class mainframe {
+}
